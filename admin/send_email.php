8de7da00e2973d03bc6e5e324d653d0005599f17
Robin Wieschendorf <mail@robinwieschendorf.de>
2025-11-06 16:44:58 +0100

    refactor: extract email message creation and success/error display into separate functions

--- admin/send_email.php
+++ admin/send_email.php
@@ -5,29 +5,59 @@ declare(strict_types=1);
 // MailSender-Klasse einbinden
 require_once __DIR__ . '/../includes/MailSender.php';
 
-// E-Mail-Konfiguration
-$to = 'info@modified-shop.org';
-$subject = '🎉 Grüße aus dem Admin-Bereich!';
+/**
+ * Erstellt die E-Mail-Nachricht
+ *
+ * @return string Die formatierte E-Mail-Nachricht
+ */
+function createEmailMessage(): string
+{
+    $message = "Hallo liebes Modified-Shop Team! 👋\n\n";
+    $message .= "Ein mutiger Admin hat gerade auf einen mysteriösen Button geklickt...\n\n";
+    $message .= "🎪 Witz des Tages:\n";
+    $message .= "Letzter Wunsch des Programmierers:\n";
+    $message .= "Bitte ein Bit. 🐛🐛🐛\n\n";
+    $message .= "Mit freundlichen Grüßen,\n";
+    $message .= "Ihr automatisiertes E-Mail-System\n\n";
+    $message .= "P.S.: Diese E-Mail wurde am " . date('d.m.Y \u\m H:i:s') . " Uhr versendet.\n";
+    
+    return $message;
+}
 
-// Lustige E-Mail-Nachricht
-$message = "Hallo liebes Modified-Shop Team! 👋\n\n";
-$message .= "Ein mutiger Admin hat gerade auf einen mysteriösen Button geklickt...\n\n";
-$message .= "🎪 Witz des Tages:\n";
-$message .= "Letzter Wunsch des Programmierers:\n";
-$message .= "Bitte ein Bit. 🐛🐛🐛\n\n";
-$message .= "Mit freundlichen Grüßen,\n";
-$message .= "Ihr automatisiertes E-Mail-System\n\n";
-$message .= "P.S.: Diese E-Mail wurde am " . date('d.m.Y \u\m H:i:s') . " Uhr versendet.\n";
-
-// MailSender-Instanz erstellen und E-Mail versenden
-$mailSender = new MailSender();
-
-if ($mailSender->send($to, $subject, $message)) {
+/**
+ * Gibt eine Erfolgsmeldung aus
+ *
+ * @param string $to Empfänger-Adresse
+ * @param string $subject Betreff
+ * @param string $message Nachricht
+ */
+function displaySuccessMessage(string $to, string $subject, string $message): void
+{
     echo "✅ E-Mail erfolgreich versendet an $to!<br>";
     echo "📧 Betreff: $subject<br>";
     echo "<hr>";
     echo "<h3>Nachricht:</h3>";
     echo "<pre>" . htmlspecialchars($message) . "</pre>";
-} else {
+}
+
+/**
+ * Gibt eine Fehlermeldung aus
+ */
+function displayErrorMessage(): void
+{
     echo "❌ Fehler beim Versenden der E-Mail.";
+}
+
+// E-Mail-Konfiguration
+$to = 'info@modified-shop.org';
+$subject = '🎉 Grüße aus dem Admin-Bereich!';
+$message = createEmailMessage();
+
+// MailSender-Instanz erstellen und E-Mail versenden
+$mailSender = new MailSender();
+
+if ($mailSender->send($to, $subject, $message)) {
+    displaySuccessMessage($to, $subject, $message);
+} else {
+    displayErrorMessage();
 }
